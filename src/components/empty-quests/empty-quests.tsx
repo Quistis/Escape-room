95f3ccbca5edbe85505f80b15e1a5c88c0cda7d4
Quistis <suicidexhelia@gmail.com>
2024-04-26 14:54:09 +0500

    Убрал инлайновые стили из EmptyQuests

--- src/components/empty-quests/empty-quests.tsx
+++ src/components/empty-quests/empty-quests.tsx
@@ -1,7 +1,7 @@
 import './empty-quests.css';
 
 const EmptyQuests = (): JSX.Element => (
-  <h2 className="empty-quests" style={{textAlign: 'center', gridColumn: '1 / -1', paddingTop: '70px'}}>🥺👉👈Тут пока ничего нет👉👈🥺</h2>
+  <h2 className="empty-quests">🥺👉👈Тут пока ничего нет👉👈🥺</h2>
 );
 
 export default EmptyQuests;
